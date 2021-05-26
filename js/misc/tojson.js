a2b2f95ee61eb96627480a7c14c7576f94a3b979
Tony Houghton <h@realh.co.uk>
2021-05-26 22:34:46 +0100

    Qualify use of byteArray.fromString()

--- js/misc/tojson.js
+++ js/misc/tojson.js
@@ -1,6 +1,6 @@
 import GLib from "gi://GLib";
 import {loadGbx} from "../core/load.js";
-const {fromString} = imports.byteArray;
+const byteArray = imports.byteArray;
 
 // gjs has a global log and no console
 if (!globalThis.console) {
@@ -9,7 +9,7 @@ if (!globalThis.console) {
 
 const gbx = loadGbx(ARGV[0]);
 const s = JSON.stringify(gbx, null, 2);
-const bytes = fromString(s);
+const bytes = byteArray.fromString(s);
 if (!GLib.file_set_contents(ARGV[1], bytes)) {
     throw Error(`Unable to write to ${ARGV[1]}`);
 }
