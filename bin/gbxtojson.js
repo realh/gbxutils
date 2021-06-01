a13e68dcadc2baa0cc74524e2436d49501d7778b
Tony Houghton <h@realh.co.uk>
2021-06-01 14:17:19 +0100

    Replaced load.js with gbxfile.js

--- bin/gbxtojson.js
+++ bin/gbxtojson.js
@@ -1,7 +1,7 @@
-import {loadGbx} from "../lib/load.js";
+import {loadGbxFile} from "../lib/gbxfile.js";
 import {saveText} from "../lib/loadsave.js";
 
 const args = process.argv;
-const gbx = loadGbx(args[2]);
+const gbx = loadGbxFile(args[2]);
 const s = JSON.stringify(gbx, null, 2);
 saveText(args[3], s);
