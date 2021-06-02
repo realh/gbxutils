acaf3aea9f50f7dc6b60e1bbe71b8f395a3dd228
Tony Houghton <h@realh.co.uk>
2021-06-02 18:33:11 +0100

    gbxurls doesn't decode body

--- bin/gbxurls.js
+++ bin/gbxurls.js
@@ -1,6 +1,6 @@
 import {loadGbxFile, saveGbxFile} from "../lib/gbxfile.js";
 
 const args = process.argv;
-const gbx = loadGbxFile(args[2]);
+const gbx = loadGbxFile(args[2], false);
 
 saveGbxFile(args[3], gbx);
