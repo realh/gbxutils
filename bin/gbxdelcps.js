e4a56230c3c1893463762b56c4e10d62b5464015
Tony Houghton <h@realh.co.uk>
2021-06-13 15:54:56 +0100

    gbxdelcps seems to work

--- bin/gbxdelcps.js
+++ bin/gbxdelcps.js
@@ -1,6 +1,8 @@
 import {loadGbxFile, saveGbxFile} from "../lib/gbxfile.js";
-import {cutBlocks} from "../lib/tools.js";
+import {cutBlocks, findChunk} from "../lib/tools.js";
 
+// The numbers are the number (modulo 4) of 90deg rotations to apply to a
+// replacement block to match the checkpoint's orientation
 const equivalents = {
     "StadiumRoadMainCheckpoint": ["StadiumRoadMain", 0],
     "StadiumRoadMainCheckpointUp": ["StadiumRoadMainSlopeStraight", 0],
@@ -16,21 +18,28 @@ const equivalents = {
     "StadiumRoadDirtHighCheckpoint": ["StadiumRoadDirtHigh", 0],
 }
 
+let cpsChunk;
+
 function filter(block) {
     const name = block.blockName[0];
     if (!name.includes("Checkpoint")) {
-        console.log(`${name} is not a checkpoint`);
         return false;
     }
     const replacement = equivalents[name];
     if (!replacement) {
-        //console.log(`  Deleting non-replaceable checkpoint ${name}`); 
+        console.log(`  Deleting non-replaceable checkpoint ${name} ` +
+                `at (${block.x}, ${block.y}, ${block.z})`); 
+        // Deleting this block will automatically remove it from cpsChunk
         return true;
     }
-    //console.log(`  Replacing checkpoint ${name} with ${replacement[0]} ` +
-    //        `rotated by ${replacement[1]}`); 
+    console.log(`  Replacing checkpoint ${name} with ${replacement[0]} ` +
+            `rotated by ${replacement[1]} ` +
+            `at (${block.x}, ${block.y}, ${block.z})`); 
     block.blockName[0] = replacement[0];
     block.rotation = (block.rotation + replacement[1]) % 4;
+    // We're changing the block, not removing it, so we also need to remove it
+    // from cpsChunk
+    cpsChunk.removeCheckpointAt(block.x, block.y, block.z);
     return false;
 }
 
@@ -43,6 +52,7 @@ ${args[0]} ${args[1]} 'Input.Challenge.Gbx' 'Output.Challenge.Gbx'`);
         return;
     }
     const gbx = loadGbxFile(args[2], true);
+    cpsChunk = findChunk(gbx.body, 0x03043017);
     cutBlocks(gbx, filter);
     saveGbxFile(args[3], gbx);
 }
