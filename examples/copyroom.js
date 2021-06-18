e3bac513e50712c667abecb60617e921f3ca133c
Tony Houghton <h@realh.co.uk>
2021-06-18 14:53:21 +0100

    Varius fixes in tools.js and copyroom.js

--- examples/copyroom.js
+++ examples/copyroom.js
@@ -1,11 +1,11 @@
 // This example deletes all the blocks within a rectangular cuboid
 import {userTransformMain} from "../lib/transform.js";
-import {copyBlocks, rotateSelection, translateSelection, pasteBlocks} from
-    "../lib/tools.js";
+import {copyBlocks, rotateSelection, translateSelection, pasteBlocks,
+    getSelectionBounds} from "../lib/tools.js";
 
 // These ranges are inclusive
 const SRC_XRANGE = [13, 31];
-const SRC_ZRANGE = [0, 12];
+const SRC_ZRANGE = [19, 31];
 
 // This filter selects blocks based on their position. All blocks within the
 // above x and z ranges, at any height (y), will be deleted
@@ -19,10 +19,14 @@ function filter(block) {
 // map and pastes it there.
 function copyTransform(gbxFile) {
     const seln = copyBlocks(gbxFile, filter);
+    console.log(
+        `Selected ${seln.length} blocks in bounds ${getSelectionBounds(seln)}`);
     rotateSelection(seln, 2, false);
-    // After rotation the blocks will be in range x: [-31, -13], z: [-12, 0];
-    // translate them to DEST_RANGE x: [13, 31], z: [19, 31]
+    console.log(`Rotated selection has bounds ${getSelectionBounds(seln)}`);
+    // After rotation the blocks will be in range x: [-31, -13], z: [-31, -19];
+    // translate them to DEST_RANGE x: [13, 31], z: [0, 12]
     translateSelection(seln, 13 + 31, 0, 31);
+    console.log(`Translated selection has bounds ${getSelectionBounds(seln)}`);
     pasteBlocks(gbxFile, seln);
 }
 
